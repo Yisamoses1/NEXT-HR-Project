5e6618d82956b181f83f85debdd5c3f5b00fb0bd
Yisamoses1 <yisarasaq2018@gmail.com>
2024-12-25 01:11:52 +0100

    Addition of tables to the project

---
+++ src/employee/dto/create-employee.dto.ts
@@ -0,0 +1,32 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator"
+import { Status } from "./statusEnum"
+import { ContractType } from "./contractEnum"
+
+export class CreateEmployeeDto {
+    @IsString()
+    @IsNotEmpty()
+    department: string
+
+    @IsString()
+    @IsNotEmpty()
+    position: string
+
+    @IsString()
+    @IsNotEmpty()
+    salary: string
+
+    @IsEnum(Status, {message: "Status must be ACTIVE, IN_ACTIVE, SUSPENDED OR TERMINATED"})
+    status: Status
+    
+    @IsOptional()
+    @IsUUID()
+    managerId?: string
+
+    @IsEnum(ContractType, {message: "Contract type must be FULL_TIME OR CONTRACT"})
+    contractType: ContractType
+
+    @IsString()
+    @IsOptional()
+    endDate?: string
+
+}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+    constructor (config: ConfigService) {
+        super({
+            datasources: {
+                db: {
+                    url:config.get("DATABASE_URL")
+                }
+            }
+        })
+        console.log(config.get("DATABASE_URL"))
+    }
+}
