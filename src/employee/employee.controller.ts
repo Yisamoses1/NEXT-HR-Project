2b9812b370a84885ed30e475f97492edd3064aa6
Yisamoses1 <yisarasaq2018@gmail.com>
2025-01-07 11:54:06 +0100

    Adding swagger documentation

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Employee's account")
 @Controller('employee')
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
 
   @Post("create")
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({summary: "Create an employee's account"})
+  @ApiResponse({status:201, description: "Employee created successfully"})
+  @ApiResponse({status: 401, description: "Bad request"})
   createEmployee(@Body() dto: CreateEmployeeDto) {
     return this.employeeService.createEmployee(dto)
   }    
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-
+@ApiTags("Authentication")
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post("signup")
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({description: "Register a new user"})
+  @ApiResponse({status: 201, description: "User created successfully"})
+  @ApiResponse({status: 401, description: "Bad Request"})
   async signUp(@Body() dto: CreateUserDto) {
     try {
       const newUser = await this.userService.signUp(dto)
