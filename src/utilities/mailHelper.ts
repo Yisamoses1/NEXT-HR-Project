7d74885532602ed6b9e512b6249edde63187252b
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-02-02 07:55:46 +0100

    chore: fixes

--- src/utilities/mailHelper.ts
+++ src/utilities/mailHelper.ts
@@ -1,30 +1,26 @@
 import * as nodemailer from 'nodemailer';
 
-export async function SendMail (
-    to: string,
-    subject: string,
-    text: string
-) {
-    //configure the email service
-    const transporter = nodemailer.createTransport({
-        service: 'gmail', 
-        auth: {
-          user: process.env.EMAIL_USER, 
-          pass: process.env.EMAIL_PASSWORD 
-        }
-    })
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        text
-      };
+export async function SendMail(to: string, subject: string, text: string) {
+  //configure the email service
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
 
-      try {
-        await transporter.sendMail(mailOptions);
-        console.log('Email sent successfully');
-      } catch (error) {
-        console.error('Error sending email: ', error);
-        throw new error('Email not sent');
-      }
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email: ', error);
+    throw new error('Email not sent');
+  }
 }
